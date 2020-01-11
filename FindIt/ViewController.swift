d429bd7737bbc48749082b5b1888e52ce8ea5b84
HaTK <tkh219@gmail.com>
2020-01-11 16:51:42 +0700

    Create a project

---
+++ FindIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindIt
+//
+//  Created by Trần Khánh Hà on 12/31/19.
+//  Copyright © 2019 Trần Khánh Hà. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
